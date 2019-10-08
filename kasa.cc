fa1f4acff96e63b236c3db602f7f46e660de4c07
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2019-10-08 22:33:36 +0200

    .

--- kasa.cc
+++ kasa.cc
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using Stop = std::pair<std::string, uint>;
+using Route = std::vector<Stop>;
+using Timetable = std::unordered_map<uint, Route>;
+
+void parseInputLine(const std::string& line) {
+    std::vector<std::string> tokens;
+
+    std::istringstream iss(line);
+    for (std::string s; iss >> s;) {
+        tokens.push_back(s);
+    }
+}
+
+void handleInput() {
+    std::string buffer;
+
+    while(std::getline(std::cin, buffer)) {
+        parseInputLine(buffer);
+    }
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+    handleInput();
+    return 0;
 }
