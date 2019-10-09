0b4330e90cc0fb015330e5682dbe60fc9aba921f
Jadw1 <jadw1_1@o2.pl>
2019-10-10 00:42:36 +0200

    first sketch

--- kasa.cc
+++ kasa.cc
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <set>
+#include <variant>
 
 using Ticket = std::tuple<std::string, unsigned long long, unsigned long long>;
 using TicketSet = std::set<Ticket>;
@@ -12,20 +13,169 @@ using Stop = std::pair<const std::string&, uint>;
 using Route = std::vector<Stop>;
 using Timetable = std::unordered_map<uint, Route>;
 
-void parseInputLine(const std::string& line) {
-    std::vector<std::string> tokens;
+using TourStop = std::pair<std::string, uint>;
+using Tour = std::vector<TourStop>;
 
-}
+/*
+ * uint -> for counted ride time
+ * std::string -> stop name where passenger has to wait
+ * bool -> false value when there is error in ride
+ */
+using FindingResult = std::variant<uint, std::string, bool>;
 
-void handleInput() {
-    std::string buffer;
+FindingResult countTime(const Tour& tour, const Timetable& tTable) {
+  uint totalTime = 0;
+  uint arrivalTime = 0;
 
-    while(std::getline(std::cin, buffer)) {
-        parseInputLine(buffer);
+  for(std::size_t i = 0; i < tour.size() - 1; i++) {
+    auto& actual = tour[i];
+    auto& next = tour[i + 1];
+
+    const auto& actualName = actual.first;
+    const auto& line = tTable.at(actual.second);
+    const auto& nextName = next.first;
+
+    bool started = false;
+    //TODO: check if line ended
+    for(auto& stop: line) {
+      if(!started) {
+        if(actualName != stop.first)
+          continue;
+        else {
+          if(i != 0 && arrivalTime != stop.second) {
+            //TODO: check if arrival time > stop.second
+            return FindingResult(stop.first);
+          }
+          arrivalTime = stop.second;
+          started = true;
+        }
+      }
+      else {
+        if(nextName != stop.first)
+          continue;
+        else {
+          uint time = stop.second - arrivalTime;
+          arrivalTime = stop.second;
+          totalTime += time;
+          break;
+        }
+      }
     }
+
+    if(!started) {
+      FindingResult(false);
+    }
+  }
+
+  return FindingResult(totalTime);
 }
 
 int main() {
-    handleInput();
-    return 0;
+    //handleInput();
+
+  //TEST
+
+  StopNameSet names;
+  Route r;
+
+  std::string n1("name1");
+  names.insert(n1);
+  Stop s1 = Stop(n1, 10);
+
+  std::string n2("name2");
+  names.insert(n2);
+  Stop s2 = Stop(n2, 20);
+
+  std::string n3("name3");
+  names.insert(n3);
+  Stop s3 = Stop(n3, 30);
+
+  std::string n4("name4");
+  names.insert(n4);
+  Stop s4 = Stop(n4, 40);
+
+  std::string n5("name5");
+  names.insert(n5);
+  Stop s5 = Stop(n5, 50);
+
+  std::string n6("name6");
+  names.insert(n6);
+  Stop s6 = Stop(n6, 60);
+
+  r.push_back(s1);
+  r.push_back(s2);
+  r.push_back(s3);
+  r.push_back(s4);
+  r.push_back(s5);
+  r.push_back(s6);
+
+
+
+
+  std::string na1("line2_1");
+  names.insert(na1);
+  Stop ns1(na1, 15);
+
+  Stop ns2(n4, 40);
+
+  std::string na3("line2_3");
+  names.insert(na3);
+  Stop ns3(na3, 44);
+
+  std::string na4("line2_4");
+  names.insert(na4);
+  Stop ns4(na4, 58);
+
+  Stop ns5(n6, 70);
+
+  Route r2;
+  r2.push_back(ns1);
+  r2.push_back(ns2);
+  r2.push_back(ns3);
+  r2.push_back(ns4);
+  r2.push_back(ns5);
+
+  Timetable tt;
+  tt.insert({3, r});
+  tt.insert({5, r2});
+
+
+  TourStop ts1("name2", 3);
+  TourStop ts2("name6", 0);
+  Tour tour;
+
+  tour.push_back(ts1);
+  tour.push_back(ts2);
+
+
+  TourStop nts1("name1", 3);
+  TourStop nts2("name4", 5);
+  TourStop nts3("line2_4", 0);
+
+  Tour tur2;
+  tur2.push_back(nts1);
+  tur2.push_back(nts2);
+  tur2.push_back(nts3);
+
+
+  /*auto res = countTime(tour, tt);
+
+  std::cout << res.index() << std::endl;
+
+  if(res.index() == 0) {
+    std::cout << std::get<uint>(res);
+  }*/
+
+
+
+
+  auto res2 = countTime(tur2, tt);
+
+  std::cout << res2.index() << std::endl;
+
+  if(res2.index() == 0) {
+    std::cout << std::get<uint>(res2);
+  }
+
+  return 0;
 }
