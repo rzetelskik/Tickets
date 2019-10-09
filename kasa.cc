5461b66c2ef9def23e4f55d62859fe4d92c4dfa6
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2019-10-09 16:45:27 +0200

    .

--- kasa.cc
+++ kasa.cc
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+
+using Ticket = std::tuple<std::string, unsigned long long, unsigned long long>;
+using TicketSet = std::set<Ticket>;
+using StopNameSet = std::unordered_set<std::string>;
+using Stop = std::pair<const std::string&, uint>;
+using Route = std::vector<Stop>;
+using Timetable = std::unordered_map<uint, Route>;
+
+void parseInputLine(const std::string& line) {
+    std::vector<std::string> tokens;
+
+}
+
+void handleInput() {
+    std::string buffer;
+
+    while(std::getline(std::cin, buffer)) {
+        parseInputLine(buffer);
+    }
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+    handleInput();
+    return 0;
 }
